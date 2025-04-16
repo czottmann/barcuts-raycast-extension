bcd5ae0ea49c08f4e303f6ddc5cf450ec6c0438d
Carlo Zottmann <carlo@zottmann.dev>
2025-04-16 21:38:19 +0200

    [NEW] Adds new commands [CHG] Moves working incoming data handling to separate command

---
+++ src/select.tsx
@@ -0,0 +1,42 @@
+import { LaunchProps, List, showToast, Toast } from "@raycast/api";
+import { useEffect, useState } from "react";
+
+type Arguments = {
+  list: string;
+};
+
+export default function Command(props: LaunchProps<{ arguments: Arguments }>) {
+  const [items, setItems] = useState<{ id: number; title: string }[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const args = props.arguments;
+    if (typeof args?.list !== "string" || args?.list === "") {
+      return;
+    }
+
+    try {
+      const list = JSON.parse(decodeURIComponent(args.list)) as string[];
+      setItems(list.map((item, index) => ({ id: index, title: item })));
+      setLoading(false);
+    } catch (error) {
+      if (error instanceof Error) {
+        showToast({
+          style: Toast.Style.Failure,
+          title: "Failed to parse list",
+          message: error.message,
+        });
+      }
+    }
+  }, [props.arguments]);
+
+  if (loading) {
+    return <List isLoading={true} />;
+  }
+
+  return (
+    <List>
+      {items.map((item) => <List.Item key={item.id} title={item.title} />)}
+    </List>
+  );
+}
---
+++ src/shared.tsx
@@ -0,0 +1,13 @@
+import { exec } from "child_process";
+
+type WorkflowType = "active" | "tagged" | "global";
+
+export function callBarCuts(type: WorkflowType) {
+  const callbackURI = encodeURIComponent(
+    "raycast://extensions/czottmann/barcuts-companion/select",
+  );
+  const barcutsURI = `barcuts-dev://raycast/workflows/${type}` +
+    `?x-success=${callbackURI}` +
+    `&x-error=${callbackURI}`;
+  exec(`open --background '${barcutsURI}'`);
+}
