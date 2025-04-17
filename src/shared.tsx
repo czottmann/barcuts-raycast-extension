f3fbaac9f8d2577f53677242f517d66d3b2b257c
Carlo Zottmann <carlo@zottmann.dev>
2025-04-17 14:07:43 +0200

    [CHG] Sets correct URI scheme protocol

--- src/shared.tsx
+++ src/shared.tsx
@@ -6,7 +6,7 @@ export function callBarCuts(type: WorkflowType) {
   const callbackURI = encodeURIComponent(
     "raycast://extensions/czottmann/barcuts-companion/select",
   );
-  const barcutsURI = `barcuts-dev://raycast/workflows/${type}` +
+  const barcutsURI = `barcuts://raycast/workflows/${type}` +
     `?x-success=${callbackURI}` +
     `&x-error=${callbackURI}`;
   exec(`open --background '${barcutsURI}'`);
